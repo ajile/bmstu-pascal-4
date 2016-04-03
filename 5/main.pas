c60ee79938bac5f52b80a519e3864955f582659e
ajile <v.v.milkov@gmail.com>
2016-04-03 23:48:39 +0300

    5

--- 5/main.pas
+++ 5/main.pas
@@ -2,10 +2,25 @@ program main;
 
 {$mode objfpc}{$H+}
 
+
+//
+// Generator:
+//
+// const list = new Array();
+// for (let i = 0; i < 40; i++) {
+//   list.push(Math.random()*100 >> 0)
+// }
+//
+
 var
-  a: integer;
+  i, counter: integer;
+  list: array [1..40] of Longint = (29, 79, 98, 15, 76, 63, 95, 12, 2, 85, 6, 85, 51, 98, 45, 85, 69, 62, 33, 34, 52, 86, 74, 47, 5, 17, 97, 75, 2, 9, 0, 97, 38, 28, 22, 74, 12, 73, 23, 66);
 begin
-  a := 1;
-  write('TEST', a);
+  counter := 0;
+  for i:=1 to length(list) do
+  begin
+    if list[i-1] > list[i] then inc(counter)
+  end;
+  write('Число инверсий: ', counter);
 end.
 
--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 * ✘ Задача 3б - есть вопрос https://github.com/ajile/bmstu-pascal-4/issues/1
 * ✓ Задача 4a - https://ideone.com/MHYSQo
 * ✓ Задача 4б - https://ideone.com/9Yhbka (для 10^3 - 60 итераций; для 10^4 - 376 итераций)
+* ✓ Задача 5 - https://ideone.com/F44XcS
+* ✘ Задача 6б - Working on
