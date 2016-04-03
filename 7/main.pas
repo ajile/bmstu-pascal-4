eac37a139375fe6f69d425b44424580c351b0029
ajile <v.v.milkov@gmail.com>
2016-04-04 01:07:25 +0300

    trash

--- 7/main.pas
+++ 7/main.pas
@@ -2,10 +2,54 @@ program main;
 
 {$mode objfpc}{$H+}
 
+type
+  TStringArray = array of string;
+
+function Explode(Ch: Char; const Text: string): TStringArray;
 var
-  a: integer;
+  i, k, Len: Integer;
+  Count: Integer;
 begin
-  a := 1;
-  write('TEST', a);
+  if Text = '' then
+  begin
+    Result := nil;
+    Exit;
+  end; // if
+  Count := 0;
+  Len := Length(Text);
+  for i := 1 to Len do
+  begin
+    if Text[i] = Ch then Inc(Count);
+  end; // for i
+  SetLength(Result, Count + 1);
+  Count := 0;
+  k := 1;
+  for i := 1 to Len do
+  begin
+    if Text[i] = Ch then
+    begin
+      Result[Count] := Copy(Text, k, i - k);
+      Inc(Count);
+      k := i + 1;
+    end; // if
+  end;
+  Result[Count] := Copy(Text, k, Len - k + 1);
+end;
+
+
+var
+  i: integer;
+  posit: integer;
+  text: String = 'Бывший защитник петербургского Зенита Константин Лепёхин поделился впечатлениями от матча второго тура российской Премьер-Лиги котором сине-бело-голубые одержали домашнюю победу над московским ЦСКА счётом прошедшей игре дубль оформил бразильский нападающий Халк';
+  words: TStringArray;
+begin
+  words := Explode(' ', text);
+
+  for i:=0 to length(words)-1 do
+  begin
+    posit := Pos(words, 'а');
+    writeLn(posit);
+  end;
+
 end.
 
--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# Программирование (Вариант 4)
+# BMSTU Pascal 4
 
-* ✓ Задача 3а - https://ideone.com/nuG9AA - не уверен, что понял задачу
-* ✘ Задача 3б - есть вопрос https://github.com/ajile/bmstu-pascal-4/issues/1
-* ✓ Задача 4a - https://ideone.com/MHYSQo
-* ✓ Задача 4б - https://ideone.com/9Yhbka (для 10^3 - 60 итераций; для 10^4 - 376 итераций)
+* ✓ Задача 3A - https://ideone.com/nuG9AA - I'm not sure understand the task
+* ✘ Задача 3B - I've a question here https://github.com/ajile/bmstu-pascal-4/issues/1
+* ✓ Задача 4A - https://ideone.com/MHYSQo
+* ✓ Задача 4B - https://ideone.com/9Yhbka (для 10^3 - 60 итераций; для 10^4 - 376 итераций)
 * ✓ Задача 5 - https://ideone.com/F44XcS
-* ✘ Задача 6б - Working on
+* ✘ Задача 6B - Working on
+* ✘ Задача 7 - I'm working on the string splitting problem
+* ✘ Задача 9 - How to do that? I've to do the last part of the task?
+* ✘ Задача 12
+* ✘ Задача 14 - mockup
+* ✘ Задача 15 - mockup
+* ✘ Задача 16 - mockup
