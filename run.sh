d43fe5efa2a9ad6731430933752852554f94daaf
ajile <v.v.milkov@gmail.com>
2016-04-03 14:43:40 +0300

    Struct

--- run.sh
+++
@@ -1,4 +0,0 @@
-echo "Compiling program $* ===================================================="
-fpc -Fudirectory -gh ./$*/main.pas
-echo "Running program $* ======================================================\n\n\n\n"
-./$*/main
