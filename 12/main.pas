47c684b4c71c445cf5ce35a82b11a8c39e8d30cd
ajile <v.v.milkov@gmail.com>
2016-04-11 16:12:41 +0300

    Another one commit

--- 12/main.pas
+++ 12/main.pas
@@ -2,10 +2,40 @@ program main;
 
 {$mode objfpc}{$H+}
 
+uses
+ Sysutils;
+
+const
+  FILE_NAME_IN = 'input.txt';
+  FILE_NAME_OUT = 'output.txt';
+
 var
-  a: integer;
+  input: TextFile;
+  output: TextFile;
+  s: string;
 begin
-  a := 1;
-  write('TEST', a);
+
+  // Откликаемся...
+  writeln('Reading the contents of file: ', FILE_NAME_IN);
+  writeln('=========================================');
+
+  // Указываем файлы, которые будем использовать
+  assignFile(input, FILE_NAME_IN); 
+  assignFile(output, FILE_NAME_OUT); 
+
+  // Открываем файл на чтение
+  reset(input); 
+
+  // Открываем файл на запись
+  rewrite(output); 
+
+  while not eof(input) do
+    begin
+      readln(input, s);
+      writeln(s);
+    end;
+
+  closeFile(input);
+  closeFile(output);
 end.
 
