bb0050b65228acf5c67b227aa4b65e0bfd916a50
ajile <v.v.milkov@gmail.com>
2016-04-18 19:22:29 +0300

    asd

--- 16/main.pas
+++ 16/main.pas
@@ -22,70 +22,128 @@ type
   RealClass = class
   private
     number: real;
+    integerPartString: string;
+    fractionPartString: string;
+    integerPart, fractionPart: integer;
   public
     constructor create(n: real);
-    function getFraction(): real;
     function getFractionLength(): integer;
-    function getInteger(): integer;
     function getIntegerLength(): integer;
+    function ratio() : double;
   end;
 
 constructor RealClass.create(n: real);
+  var
+    numberString: string;
+    i: integer;
+    reached: boolean = false;
   begin
-    number := n;
-  end;
+    numberString := FloatToStr(n);
+    for i := 1 to length(numberString) do begin
+      if numberString[i] = '.' then begin
+        reached := true;
+        continue;
+      end;
+      if not(reached) then begin
+        integerPartString := concat(integerPartString, numberString[i]);
+      end else begin
+        fractionPartString := concat(fractionPartString, numberString[i]);
+      end;
+    end;
+    integerPart := StrToInt(integerPartString);
+    fractionPart := StrToInt(fractionPartString);
+    // writeln(integerPartString);
+    // writeln(integerPart);
 
-function RealClass.getFraction() : real;
-  begin
-    // writeln( length(IntToStr(getInteger())) );
-    // writeln( IntToStr(getInteger()) );
-    // writeln( IntToStr(getInteger()) );
-    // writeln(  power(10, length(IntToStr(getInteger())))  );
-    // writeln(  frac(number) * power(10, length(IntToStr(getInteger())))  );
-    // writeln(    );
-    result := frac(number);
+    number := n;
   end;
 
 function RealClass.getFractionLength() : integer;
   begin
-    result := 1;
-  end;
-
-function RealClass.getInteger() : integer;
-  begin
-    result := trunc(number);
+    result := length(fractionPartString);
   end;
 
 function RealClass.getIntegerLength() : integer;
   begin
-    result := length(IntToStr(getInteger()));
+    result := length(integerPartString);
   end;
 
+function RealClass.ratio() : double;
+ begin
+   
+ end;
 // =============================================================================
 
 // Класс массив вещественных чисел переменной длины. Параметры: количество чисел
 // и массив. Метод определения суммы введенных чисел и отношения количества цифр
 // в целой и дробной частях.
 
-// @TODO
+type
+  RealClassList = class
+  public
+    list: array of RealClass;
+    constructor create();
+    procedure add(scObject: RealClass);
+    function sumRatio() : double;
+    function sum() : double;
+  end;
+
+constructor RealClassList.create();
+  begin
+  end;
+
+procedure RealClassList.add(scObject: RealClass);
+ var
+   l: integer = 0;
+ begin
+   l := length(list) + 1;
+   setlength(list, l);
+   list[l] := scObject;
+   // list[l+1] := scObject;
+   // writeln(scObject.getIntegerLength(), '-', scObject.getFractionLength());
+   // writeln(length(list));
+ end;
+
+function RealClassList.sumRatio() : double;
+ var
+  i: integer;
+  res: double = 0;
+ begin
+   for i := 1 to length(list) do begin
+     res := res + (list[i].getIntegerLength() / list[i].getFractionLength());
+   end;
+   result := res;
+ end;
+
+function RealClassList.sum() : double;
+ var
+  i: integer;
+  res: double = 0;
+ begin
+   for i := 1 to length(list) do begin
+     res := res + list[i].number;
+   end;
+   result := res;
+ end;
 
 // =============================================================================
 
 var
-  realNumber: RealClass;
-  num: real = 11.22334455;
+  realNumberList: RealClassList;
 begin
 
-  {$IFDEF DEBUG}
-    writeln('The number program working on: ', FloatToStr(num));
-    writeln('=========================================');
-  {$ENDIF}
+  realNumberList := RealClassList.create();
+  realNumberList.add(RealClass.create(11.22334455));
+  // realNumberList.add(RealClass.create(3124.543262));
+  // realNumberList.add(RealClass.create(5432.64232));
+  // realNumberList.add(RealClass.create(12.5327347));
 
-  realNumber := RealClass.create(num);
+  writeln('Sum: ', realNumberList.sum());
+  writeln('Sum of ratio: ', realNumberList.sumRatio());
 
   {$IFDEF DEBUG}
-    writeln('The integer part of the number: ', realNumber.getInteger());
-    writeln('The fraction part of the number: ', realNumber.getFraction());
+    // writeln('The integer part of the number: ', realNumber.getIntegerLength());
+    // writeln('The fraction part of the number: ', realNumber.getFractionLength());
   {$ENDIF}
 
 end.
--- 9/main.pas
+++ 9/main.pas
@@ -1,11 +1,62 @@
 program main;
 
-{$mode objfpc}{$H+}
+const DIMENSION = 10;
+
+// Простая сортировка методом пузырька
+procedure bubblesort(var numbers: array of integer);
+var
+  i, j, tmp: integer;
+begin
+  for i := length(numbers) - 1 downto 1 do
+    for j := 1 to i do
+      if (numbers[j-1] > numbers[j]) then begin
+        tmp := numbers[j-1];
+        numbers[j-1] := numbers[j];
+        numbers[j] := tmp;
+      end;
+end;
 
 var
-  a: integer;
+  i, j: integer;
+  matrix: array[1..DIMENSION] of array[1..DIMENSION] of integer;
 begin
-  a := 1;
-  write('TEST', a);
-end.
 
+  // Наполняем матрицу случайными числами
+  for i:=1 to length(matrix) - 1 do begin
+    for j:=1 to length(matrix[i]) do begin
+      matrix[i][j] := random(10);
+    end;
+  end;
+
+  writeln('The incoming matrix:'); writeln;
+
+  // Показываем получившуюся матрицу
+  for i:=1 to length(matrix) - 1 do begin
+    for j:=1 to length(matrix[i]) do begin
+      write(matrix[i][j], ' ');
+    end;
+    writeln;
+  end;
+
+
+  writeln;
+  writeln('↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓');
+  writeln;
+
+
+  // Показываем отсортированную матрицу
+  for i:=1 to length(matrix) - 1 do begin
+
+    // Сортируем строку
+    bubblesort(matrix[i]);
+
+    // Распечатываем строку
+    for j:=1 to length(matrix[i]) do begin
+      write(matrix[i][j], ' ');
+    end;
+
+    writeln;
+
+  end;
+
+end.
