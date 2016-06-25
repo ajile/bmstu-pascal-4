fe7f48df7793a5691e11d1122b4b1492c3eb5b9b
veerok <vselivanova4@gmail.com>
2016-06-25 19:18:51 +0300

    tmp

---
+++ tmp/1.pas
@@ -0,0 +1,71 @@
+program main;
+
+const DIMENSION = 10;
+
+// Простая сортировка методом пузырька
+procedure bubblesort(var numbers: array of integer);
+var
+  i, j, tmp: integer;
+begin
+  for i := length(numbers) - 1 downto 1 do
+    for j := 1 to i do
+      if (numbers[j-1] > numbers[j]) then begin
+        tmp := numbers[j-1];
+        numbers[j-1] := numbers[j];
+        numbers[j] := tmp;
+      end;
+end;
+
+var
+  i, j: integer;
+  list: array[1..10] of integer = (4,0,1,4,2,8,3,2,1,0);
+begin
+
+  // Наполняем матрицу случайными числами
+  for i:=1 to length(list) - 1 do writeln(list[i]);
+
+  bubblesort(list);
+  writeln;
+  writeln('Отсортированный массив:');
+
+  for i:=1 to length(list) - 1 do writeln(list[i]);
+
+  writeln;
+  writeln('Без 0:');
+
+  for i:=1 to length(list) - 1 do begin
+  	if list[i] <> 0 then writeln(list[i]);
+  end;
+
+  // writeln('The incoming list:'); writeln;
+
+  // // Показываем получившуюся матрицу
+  // for i:=1 to length(list) - 1 do begin
+  //   for j:=1 to length(list[i]) do begin
+  //     write(list[i][j], ' ');
+  //   end;
+  //   writeln;
+  // end;
+
+
+  // writeln;
+  // writeln('↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓ ↓');
+  // writeln;
+
+
+  // // Показываем отсортированную матрицу
+  // for i:=1 to length(list) - 1 do begin
+
+  //   // Сортируем строку
+  //   bubblesort(list[i]);
+
+  //   // Распечатываем строку
+  //   for j:=1 to length(list[i]) do begin
+  //     write(list[i][j], ' ');
+  //   end;
+
+  //   writeln;
+
+  // end;
+
+end.
